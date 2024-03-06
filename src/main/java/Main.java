0bb72ac6ae000b2a6aacc01bd625d4bd1498350b
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-03-06 17:05:58 +0330

    last commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,7 @@
+import java.io.IOException;
+import org.json.JSONObject;
 public class Main {
-    public static void main(String[] args) {
-        // TODO --> complete main function
-        runMenu();
-    }
-    public static void runMenu() {
-        // TODO
+    public static void main(String[] args) throws IOException {
+        Menu.displayMainMenu();
     }
 }
